5db3abf34a9096c70c200b31ed5fde37abd8c2d7
wildanjr19 <wildansekolah11@gmail.com>
2025-08-28 19:18:08 +0700

    complete Naive Bayes

--- NaiveBayes/modules.py
+++ NaiveBayes/modules.py
@@ -19,13 +19,31 @@ class NaiveBayes:
             # cari mean, var, prior dari tiap fitur pada data yang termasuk kelas c (X_c)
             self._mean[c, :] = X_c.mean(axis=0)
             self._var[c, :] = X_c.var(axis=0)
+            # probabilitas awal untuk setiap kelas
             self._priors[c] = X_c.shape[0] / float(n_samples)
 
 
     def predict(self, X):
-        pass
-
+        y_pred = [self._predict(x) for x in X]
+        return y_pred
 
     # helper -> prediksi untuk satu sampel
     def _predict(self, x):
-        pass
+        posteriors = []
+
+        for idx, c in enumerate(self._classes):
+            prior = np.log(self._priors[idx])
+            class_conditional = np.sum(np.log(self._pdf(idx, x)))
+            posterior = prior + class_conditional
+            posteriors.append(posterior)
+
+        return self._classes[np.argmax(posteriors)]
+
+    # helper -> PDF/Posteriors/Conditional Probability
+    def _pdf(self, class_idx, x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+
+        numerator = np.exp(- (x - mean) ** 2 / (2 * var))
+        denominator = np.sqrt(2 * np.pi * var)
+        return numerator / denominator
