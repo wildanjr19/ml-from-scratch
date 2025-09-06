9f3a89de6b4f4c479e36c7122dd8c34a9cf1a18c
wildanjr19 <wildansekolah11@gmail.com>
2025-09-06 09:28:44 +0700

    major update

--- RandomForest/random_forest_test.py
+++ RandomForest/random_forest_test.py
@@ -1,12 +1,11 @@
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from modules import RandomForest
 
-from decision_tree import DecisionTree
-
-# akurasi
+# evaluasi
 def accuracy(y_true, y_pred):
-    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    accuracy = np.sum(y_true == y_pred) /len(y_true)
     return accuracy
 
 
@@ -15,12 +14,14 @@ data = datasets.load_breast_cancer()
 X = data.data
 y = data.target
 
+# split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
-clf = DecisionTree(max_depth=10)
+clf = RandomForest(n_trees=3, max_depth=10)
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
 
 acc = accuracy(y_test, y_pred)
+
 print("Akurasi :", acc)
