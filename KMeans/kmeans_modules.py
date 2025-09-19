fca8e8a14355ea890de8b30443f665aad96a1aff
wildanjr19 <wildansekolah11@gmail.com>
2025-09-19 19:29:49 +0700

    add KMeans

--- KMeans/kmeans_modules.py
+++ KMeans/kmeans_modules.py
@@ -33,16 +33,26 @@ class KMeans:
             # update clusters
             self.clusters = self._create_clusters(self.centroids)
 
-            # update centroids
+            if self.plot_steps:
+                self.plot()
 
+            # update centroids
+            centroids_old = self.centroids
+            self.centroids = self._get_centroids(self.clusters)
+            
             # cek apakah sudah konvergen
+            if self._is_converged(centroids_old, self.centroids):
+                break
+
+            if self.plot_steps:
+                self.plot()
 
         # kembalikan label cluster
-
+        return self._get_cluster_labels(self.clusters)
 
     # helper -> create clusters
-    """mengkluster setiap data ke centroid terdekat"""
     def _create_clusters(self, centroids):
+        """mengkluster setiap data ke centroid terdekat"""
         # buat list kosong sebanyak K, yang akan diisi indeks
         clusters = [[] for _ in range(self.K)]
         # looping setiap data pada dataset (X)
@@ -52,3 +62,47 @@ class KMeans:
             # idx (indeks data) dimasukkan ke dalam ke list clusters pada indeks centroid terdekatnya
             clusters[centroid_idx].append(idx)
         return clusters
+    
+    # helper -> closest centroid
+    def _closest_centroid(self, sample, centroids):
+        """mengembalikan indeks centroid terdekat dari data"""
+        # jarak dari sample data ke setiap centroid
+        distances = euclidean_distance(sample, centroids)
+        # dapatkan indeks centroid dengan jarak terdekat
+        closest_index = np.argmin(distances)
+        return closest_index
+    
+    # helper -> get centroids
+    def _get_centroids(self, clusters):
+        """menghitung centorid baru dari data yang sudah dikluster"""
+        # array kosong [k, n_features]
+        centroids = np.zeros((self.K, self.n_features))
+        # hitung mean fitur di tiap kluster, lalu assign ke centroids
+        for cluster_idx, cluster in enumerate(clusters):
+            cluster_mean = np.mean(self.X[cluster], axis=0)
+            centroids[cluster_idx] = cluster_mean
+        return centroids
+    
+    # helper -> is converged
+    def _is_converged(self, centroids_old, centroids):
+        """cek konvergensi. jika jarak antara centroids lama dan baru 0 maka konvergen (True)"""
+        # hitung jarak antara centroids lama dan baru pada tiap kluster
+        distances = [
+            euclidean_distance(centroids_old[i], centroids[i]) for i in range(self.K)
+        ]
+        return sum(distances) == 0
+    
+    # helper -> get cluster labels
+    def _get_cluster_labels(self, clusters):
+        """setiap sampel data akan memiliki label kluster yang sesuai"""
+        # inisialisasi array kosong
+        labels = np.empty(self.n_samples)
+
+        # clusters -> list berisi indeks dari data
+        
+        # looping untuk tiap kluster
+        for cluster_idx, cluster in enumerate(clusters):
+            # looping untuk tiap indeks data pada kluster
+            for sample_idx in cluster:
+                labels[sample_idx] = cluster_idx
+        return labels
